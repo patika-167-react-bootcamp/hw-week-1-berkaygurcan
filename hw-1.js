913ef9b832fd88800dd63ead1e7f6f2bbc849f0c
Berkay Gürcan <46679861+berkaygurcan@users.noreply.github.com>
2022-02-07 19:39:45 +0300

    remove,copy fonksiyonları çalışıyor

--- hw-1.js
+++ hw-1.js
@@ -50,16 +50,81 @@ function move(targetfileId, targetFolderId) {
     console.log("taşıma işlemi başarılı")
 }
 
+
 function copy (targetfileId, targetFolderId) {
-  
+
+  //dosya arama işlemi
+  let targetFile = null;
+  let targetFolder = null;
+
+  // hedef dosyaya ulaşma
+
+  for(const folder of folders){
+     const {id, name, files} = folder //destruction
+     if(files) { //dosyalar varsa dolanacak
+      targetFile = (files.find( file => file.id === targetfileId))
+      break; 
+     }
+  }
+
+  //hedef klasöre ulaşma
+  targetFolder = folders.find(folder => folder.id === targetFolderId)
+
+  //@todo - üstteki logic move ve copy de aynı ondan bunları başka bir func içerisine alabilirim.
+
+  //ilgili dosyayı klasöre kopyalama
+  //@todo yeni dosyanın idsi değişicek ve referans bağlantısını koparmamız gerekli
+  //öncelikle aynı referans değişkeni olmamaları gerekli ondan ilgili satırın referans bağını kesmemiz gerekli.
+ 
+  CopiedTargetFile = { //yeni obje oluşturarak referans bağını kırabiliriz.
+    id: 1,
+    name: targetFile.name
+   }
+
+   //ilgili dosyayı klasöre taşıma
+   targetFolder.files.push(CopiedTargetFile)
+
+   console.log("kopyalama işlemi başarılı")
+
 }
+
+function remove(targetfileId) {
+   //dosya arama işlemi
+   let targetFile = null;
+   let targetFolder = null;
+
+  // hedef dosyaya ulaşma
+  for(const folder of folders){
+    const {id, name, files} = folder //destruction
+    if(files) { //dosyalar varsa dolanacak
+     targetFolder = folder; //geçiçi olarak atadık break ifadesi ile çıkılırsa tarfet folder en son folder yani hedef olmuş olur
+     targetFile = (files.find( file => file.id === targetfileId))
+     break; 
+    }
+ }
+
+ //dosya silme işlemi
+ //https://stackoverflow.com/questions/15287865/remove-array-element-based-on-object-property?answertab=active#tab-top
+ const index = targetFolder.files.findIndex(prop => prop.id === targetFile.id);
+ targetFolder.files.splice(index,1);
+ //console.log(targetFolder.files)
+}
+
 
 
 
 // move(17,6)
 // console.log(folders[1].files) 
 
+// copy(18,5) // kopyasını oluşturacak
 
+// folders[0].files[1].name = "deneme.jpg"
+// console.log(folders[0].files) //başarılı 
+
+// remove(17) // dosyayı silecek 
+// //başarılı
+
+removeFolder(6) //klasörü ve altındaki tüm dosyaları silecek
   
 //   move(17,6) // dosyayı klasöre taşıyacak
 //   copy(18,7) // kopyasını oluşturacak
