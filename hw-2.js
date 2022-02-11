aca1e7a502066c287c9dc91e2c1605b9cac9c64e
Berkay Gürcan <46679861+berkaygurcan@users.noreply.github.com>
2022-02-11 22:28:42 +0300

    Fonksiyon ve yorum satırları güncellendi

--- hw-2.js
+++ hw-2.js
@@ -4,14 +4,14 @@ strList = ["berkay", "uTku", "FuaT", "berat", "iLHAn", "batuhan"];
 Array.prototype.includesCi = function (targetItem) {
 
     for(item of strList) {
-        if(typeof(item) === "string" && item.trim().toLowerCase() === targetItem.trim().toLowerCase()) { //trim ile ekstra sağ ve soldaki boşluklardan kurtulduk.
-            return true // ifade bulundu true döner
+        if(( typeof(item) === "string" && item.trim().toLowerCase() ) === (typeof(targetItem) === "string" && targetItem.trim().toLowerCase())) { //trim ile ekstra sağ ve soldaki boşluklardan kurtulduk.
+            return true // ifade eşleirse return döner, Returns true if there is a match
         }
 
     }
-    return false // eğer eşdeğer bir ifade bulamadıysa false döner
+    return false // eğer eşdeğer bir ifade bulamadıysa false döner , returns false if no equivalent expression was found
     
 }
 
-console.log(strList.includesCi("UtKU"))  //true
+console.log(strList.includesCi("Utku"))  //true
 console.log(strList.includesCi("FuarT")) //false
